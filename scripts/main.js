5957a6454096e2a18572391cdd759ab2ad0ac1f0
Bethany Priode <bpriode@gmail.com>
2017-08-04 16:49:03 -0400

    fixed aces

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,6 @@
 
    Also remember, that the parameter `hand` will be an array, so
    you'll need to parse through that first before you can start to
-   write your logic.
 */
 
 
@@ -14,23 +13,24 @@
 function handValue (hand){
 
   let value = 0;
+  let acePlayed = false;
 
   for (var i = 0; i < hand.length; i++) {
     if (hand[i]==="K" || hand[i]==="Q" || hand[i]==="J"){
       value +=10;
-    }else if( hand[i] === "A" && value>=15){
-      value +=1;
-    }else if( hand[i] === "A" && value<=10 && value>=7 && value!==8){
-      value +=11;
-    }else if( hand[i] === "A" && value>=8){
-      value +=1;
+    }else if (hand[i] === "A") {
+        value +=11;
+        acePlayed = true;
     }else{
       value += Number(hand[i]);
     }
-
+    if ((value > 21) && (acePlayed = true)) {
+      value -= 10;
+    }
   }
     return value;
 }
+
 
 
 
