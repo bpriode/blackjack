1d76f09141eeb66556eac34982ffcaaaca982960
Bethany Priode <bpriode@gmail.com>
2017-08-03 20:29:08 -0400

    turning in

--- scripts/main.js
+++ scripts/main.js
@@ -5,16 +5,36 @@
    to watch the assertions pass as you write your code.
 
    Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   you'll need to parse through that first before you can start to
    write your logic.
 */
 
 
-function handValue (hand) {
 
+function handValue (hand){
 
-  return;
+  let value = 0;
+
+  for (var i = 0; i < hand.length; i++) {
+    if (hand[i]==="K" || hand[i]==="Q" || hand[i]==="J"){
+      value +=10;
+    }else if( hand[i] === "A" && value>=15){
+      value +=1;
+    }else if( hand[i] === "A" && value<=10 && value>=7 && value!==8){
+      value +=11;
+    }else if( hand[i] === "A" && value>=8){
+      value +=1;
+    }else{
+      value += Number(hand[i]);
+    }
+
+  }
+    return value;
 }
+
+
+
+
 
 
 /* -----  Hints ------
